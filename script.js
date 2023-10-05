5b9cb2fa2f1b24608b3ab3cfc1be404f841cb991
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-10-05 21:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,52 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-	const priceElements = document.querySelectorAll(".prices");
-
-    // Calculate the total price
-    let totalPrice = 0;
-    priceElements.forEach(priceElement => {
-        // Parse the price text to a float and add it to the total price
-        totalPrice += parseFloat(priceElement.textContent);
-    });
-
-    // Create a new row for total price
-    const totalRow = document.createElement("tr");
-	totalRow.id="ans";
-    const totalCell = document.createElement("td");
-
-    // Set the colspan attribute to span the entire row
-    totalCell.setAttribute("colspan", "2");
-
-    // Set the text content of the total cell
-    totalCell.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
-
-    // Append the total cell to the total row
-    totalRow.appendChild(totalCell);
-
-    // Append the total row to the table body
-    const tableBody = document.querySelector("tbody");
-    tableBody.appendChild(totalRow);
+	const prices = document.querySelectorAll('.price');
+  
+  // Calculate total price by summing up the prices
+  let totalPrice = 0;
+  prices.forEach(price => {
+    totalPrice += parseInt(price.textContent);
+  });
+  
+  // Create a new row for total price
+  const newRow = document.createElement('tr');
+  const itemNameCell = document.createElement('td');
+  itemNameCell.textContent = 'Total Price';
+  newRow.appendChild(itemNameCell);
+  
+  const totalPriceCell = document.createElement('td');
+  totalPriceCell.textContent = totalPrice;
+  newRow.appendChild(totalPriceCell);
+  
+  // Add the new row to the table
+  document.querySelector('table').appendChild(newRow);
   
 };
 
 getSumBtn.addEventListener("click", getSum);
 
-
-
-// Get all elements with class name "prices" within elements with class name "item"
-const prices = document.querySelectorAll('.item .prices');
-
-// Calculate total price
-let totalPrice = 0;
-prices.forEach(price => {
-    totalPrice += parseFloat(price.textContent); // Convert the price to a number and add it to the total
-});
-
-// Create a new row for the total price
-const totalRow = document.createElement('tr');
-totalRow.innerHTML = `<td colspan="2">Total Price: $${totalPrice}</td>`; // Assuming you want to display the total price in a new row
-
-// Append the new row to the table
-const table = document.querySelector('table');
-table.appendChild(totalRow);
